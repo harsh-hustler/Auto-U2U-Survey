a95dd6ff5925ff5203b40a826b5611186d26a9dd
Harsh <harshraj.8271@hotmail.com>
2021-02-01 20:17:40 +0530

    Done some changes in passcode to not provide authencation to others in my hosted mail i'd

--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@ import smtplib
 
 def send_email(email, height, average_height, count):
     from_email= "database.web.app1@gmail.com"
-    from_password="abcqwerty123"
+    from_password=""
     to_email=email
 
     subject="Height data"
